eae434b87fd2e9eceab23fabca740a9a43adf3ee
wimade <wtlab2020@gmail.com>
2022-11-06 23:46:20 +0000

    wireless

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,11 +3,11 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
 
-        length = len(my_list)
+    length = len(my_list)
 
-        if idx > length - 1:
-            return (my_list)
-
-        my_list[idx] = element
-
+    if idx > length - 1:
         return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
 
     new_list = my_list[:]
 
-     if 0 <= idx < length:
-	 new_list[idx] = element
-     return (new_list)
+    if 0 <= idx < length:
+        new_list[idx] = element
+    return (new_list)
